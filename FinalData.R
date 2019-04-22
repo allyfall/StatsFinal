634c724e2bdf83c391721510b5d9cb38494813d1
Ally Faller <alfa8405@colorado.edu>
2019-04-22 10:02:41 -0600

    did some initial plotting

--- FinalData.R
+++ FinalData.R
@@ -1,4 +1,5 @@
 # StatsFinal
+###Note: you need to set working directory to calCOFI for this to work.
 #data: bottle.csv and cast.csv
 ##reading in the csv files
 #bottle is the data from each cast
@@ -10,8 +11,19 @@ str(castData)
 
 #What variables do we want to look at here? 
 View(bottleData)
-#We want variables:Btl_Cnt:2 Depthm:5, T_degC:6, Salnty:7, O2ml_L:8 O2Sat:10, ChlorA:22, Phaeop:24 PO4uM:26 SiO3uM:28 NO2uM:30 NO3uM:32 NH3uM:34 C14As1:36 C14As2:39 DarkAs:42 MeanAs:45 LightP:49 R_Depth: 50, R_TEMP:51, R_SALINITY:53, R_SIGMA:54, R_O2:57 R_O2Sat:58,
+#We want variables:Btl_Cnt:2 Depthm:5, T_degC:6, Salnty:7, 
+#O2ml_L:8 O2Sat:10, ChlorA:22, Phaeop:24 PO4uM:26 SiO3uM:28 
+#NO2uM:30 NO3uM:32 NH3uM:34 C14As1:36 C14As2:39 DarkAs:42
+#MeanAs:45 LightP:49 R_Depth: 50, R_TEMP:51, R_SALINITY:53, 
+#R_SIGMA:54, R_O2:57 R_O2Sat:58,
 #R_SIO3:59, R_PO4:60, R_NO3:61, R_NO2:62, R_NH4:63, R_CHLA:64, R_PHAEO:65, R_PRES:66
 #R_SAMP:67
 
+varint <- c(2,5,6,7,8,10,22,24,26,28,30,32,34,36,39,42,45,49,50,51,53,
+            54,57,58,59,60,61,62,63,64,65,66,67)
+bottleWanted <- bottleData[,varint]
 #Starting some Preliminary Exploration: 
+plot(T_degC~Depthm, data=bottleWanted)
+plot(Phaeop~Depthm, data=bottleWanted)
+plot(ChlorA~Depthm, data=bottleWanted)
+#note: we have more info on ChlorA than Phaeop (Whatever that means?)
