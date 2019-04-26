35cdc8a6da58304bb2e1632c7d149c7f6ac1883d
Ally Faller <alfa8405@colorado.edu>
2019-04-25 21:38:28 -0600

    started building ChlorA models

--- ChlorA/ChlorA_Ally.R
+++ ChlorA/ChlorA_Ally.R
@@ -14,7 +14,6 @@ bottleData <- read.csv("bottle.csv")
 varint <- c(2,5,6,7,10,22,24,26,28,30,32,34,36,39,42,45,49,50,51,53,
             54,57,58,59,60,61,62,63,64,65,66,67,72,73)
 bottleWanted <- bottleData[,varint]
-
 ChlorA <- bottleWanted[,6]
 
 #nutrient names: PO4uM:26 SiO3uM:28 
@@ -31,4 +30,48 @@ nut_data <- bottleWanted[,nutrient_variables]
 env_variables <- c(1:5, 15:23, 31:34)
 env_data <- bottleWanted[,env_variables]
 
+##training and testing data:
+set.seed(2019)
+n <- 864863*.7
+train_ind <- sample(1:864863, size = n, replace = FALSE)
+nut_train <- nut_data[train_ind,]
+ChlorA_train <- ChlorA[train_ind]
+nut_test <- nut_data[-train_ind,]
+ChlorA_test <- ChlorA[-train_ind]
+
+
 #alright. Lets start some modeling
+library(leaps)
+best.ChlorA.Nut <- regsubsets(ChlorA~.,data=nut_data, nvmax=10)
+summary(best.ChlorA.Nut)
+bCN <- summary(best.ChlorA.Nut)
+which.min(summary(best.ChlorA.Nut)$bic)
+par(mfrow=c(2,3))
+plot(bCN$rss)
+plot(bCN$rsq)
+plot(bCN$adjr2)
+plot(bCN$cp)
+plot(bCN$bic)
+#So the model with six features is the best: 
+#P04, SI03, NO2, NO3, NH3, C14As2
+#making the linear model
+names(nut_data)
+nut_best_mod <- lm(ChlorA_train~PO4uM+SiO3uM+NO2uM+NO3uM+NH3uM+C14As2, data=nut_train)
+summary(nut_best_mod)
+#how good is model fit?
+summary(nut_best_mod)$r.squared
+#Residual Mean Sq Error: 93.07%
+summary(nut_best_mod)$sigma 
+#Diagnostics:
+par(mfrow=c(2,2))
+plot(nut_best_mod)
+
+#Predicting: This is not working rn. 
+predict(nut_best_mod, newdata = nut_test,interval = "prediction")
+
+#Cross validation:
+best_nut_sub <- regsubsets(ChlorA_train~., data=nut_train, nvmax=7)
+summary(best_nut_sub)
+which.min(summary(best_nut_sub)$bic)
+#still 6. Cool.
+
